954a8d6923b2180370fd977885ec2ae843f1cb01
Anthony DeMeo <anthonydemeo@anthonys-mbp.home>
2021-08-04 18:57:36 -0400

    get rid of unused imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './pages/Home'
 import Main from './components/Main';
 import './styles.sass'
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
-import HamburgerMenu from 'react-hamburger-menu'
 
 const Home = (props) => {
     return (
-    <>
-    <p id="pageLocation">Home</p>
+    <div className="home">
+        <p id="pageLocation">Home</p>
+        <div className="bannerContainer">
+            <img src='https://res.cloudinary.com/ademeo/image/upload/v1628111347/project2/UGK_ART_m6mzk2.jpg' alt="UGK Art"/>
+        </div>
         <div className="welcome">
             <h1>Welcome to Pocket Full of Stones, a weekly newsletter about (mostly) American rap music and the people who bring it into existence. </h1>
         </div>
-    </>)
+    </div>)
 }
 
 export default Home
